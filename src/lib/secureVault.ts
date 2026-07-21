353268727e13ea50cae9a4beb8f7dce974f50c60
yonoapptransparency <yonorummynewstore@gmail.com>
2026-07-21 19:04:00 +0530

    K

--- src/lib/secureVault.ts
+++ src/lib/secureVault.ts
@@ -1,1 +1,2 @@
-export const ENCRYPTED_LINKS = "U2FsdGVkX1/pi+uj/duthatuXOpCX+sz82yF51TR7W4=";
+export const IS_SEALED = true;
+export const ENCRYPTED_LINKS = "U2FsdGVkX19O0X1zEwB1f7aV0qV4QZ+5aJc="; // Dummy encrypted empty array
