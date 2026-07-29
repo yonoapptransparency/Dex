51683160e94e8cf2ec6d673f54e21dde42ada8c4
yonoapptransparency <yonorummynewstore@gmail.com>
2026-07-29 10:06:04 +0530

    Admin Release: Secure vault synchronization for Dex

--- src/lib/secureVault.ts
+++ src/lib/secureVault.ts
@@ -1,5 +1,1 @@
-// SECURE VAULT - DO NOT EDIT MANUALLY
-export const IS_SEALED = true;
-export const ENCRYPTED_LINKS = "";
-export const secureVault = { IS_SEALED, ENCRYPTED_LINKS };
-export default secureVault;
+export const ENCRYPTED_LINKS = "U2FsdGVkX18d5rRZoJu4KgBrM2jS8GecVHhPXeTva+4=";
